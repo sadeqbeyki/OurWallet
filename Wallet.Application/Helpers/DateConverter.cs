a2e6506bd92972278a5d2e9db721a1f3fef2f715
mardibak <mardibaki@gmail.com>
2024-01-03 21:20:36 +0330

    DateCOnvertor and RtlMessageBox movewd from old framework

---
+++ Wallet.Application/Helpers/DateConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Wallet.Application.Helpers
+{
+    public static class DateConverter
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(value) + "/" + pc.GetMonth(value).ToString("00") + "/" + pc.GetDayOfMonth(value).ToString("00");
+        }
+        public static DateTime ToMiladi(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, new PersianCalendar());
+        }
+    }
+}
